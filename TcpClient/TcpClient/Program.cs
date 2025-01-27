d2da3f77f18ee57d29cf8f1fa17d6f5d1d414843
Tuan Muhammad Aidiel <tuanaidiel@gmail.com>
2025-01-27 16:41:56 +0800

    Add files via upload

---
+++ TcpClient/TcpClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyTcpClient
+{
+    class TcpClientProgram
+    {
+        static void Main(string[] args)
+        {
+            //Connect to server
+            using (TcpClient client = new TcpClient ("127.0.0.1", 8001))
+            using (NetworkStream stream = client.GetStream())
+            {
+                string message = "Hello Tuan Muhammad Aidiel!";
+                byte[] data = Encoding.UTF8.GetBytes(message);
+
+                stream.Write(data, 0, data.Length);
+                Console.WriteLine("Message sent to server");
+                Console.ReadLine();
+            }
+        }
+    }
+}
