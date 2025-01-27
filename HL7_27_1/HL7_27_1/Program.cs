947797fb305d1499f1e6e443b42d9c19d34be8b6
Tuan Muhammad Aidiel <tuanaidiel@gmail.com>
2025-01-27 17:25:35 +0800

    Add files via upload

---
+++ HL7_27_1/HL7_27_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HL7_27_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string HL7Message = "MSH|monitor|hospial|central|hospital|ORU^O01|P|2.5\n" +
+                                "PID|Bakar^Abu|M|20010306\n" +
+                                "OBX|100|mg/dL|X";
+
+            string[] segments = HL7Message.Split('\n');
+            string[] mshSegment = segments[0].Split('|');
+            string[] pidSegment = segments[1].Split('|');
+            string[] obxSegment = segments[2].Split('|');
+
+            string[] nameParts = pidSegment[1].Split('^');
+            string patientName = $"{nameParts[1]} {nameParts[0]}";
+
+            string status = obxSegment[3] == "N" ? "Normal" : "Abnormal";
+
+            System.Console.WriteLine($"Message type: {mshSegment[5]}");
+            System.Console.WriteLine($"Patient name: {patientName}");
+            System.Console.WriteLine($"Status: {status}");
+            Console.ReadLine();
+        }
+    }
+}
